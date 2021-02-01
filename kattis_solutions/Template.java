b50be36edfdb2d31c8d238c87073d7c7671c24f6
z-intercepts <zachptucker@gmail.com>
2021-02-01 10:39:29 -0500

    Add template files

---
+++ kattis_solutions/Template.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; // Import the Scanner class
+
+public class Template {
+
+    public static void main(String[] args) {
+        // Create a Scanner object
+        Scanner s = new Scanner(System.in);
+
+        // Assuming 2 String inputs & an output
+        String a = s.nextLine();
+        String b = s.nextLine();
+
+        // do things w/ a & b --> c
+        System.out.println(c);
+    }
+}
---
+++ kattis_solutions/template.py
@@ -0,0 +1,7 @@
+# Assuming 2 String inputs & an output
+a = input()
+b = input()
+c = None
+
+# do things w/ a & b --> c
+print(c)
